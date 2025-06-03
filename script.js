ecfc7877384ff67c646e9e287c36b3ad54adcfc6
Absar Ahmad <121477562+Absar11@users.noreply.github.com>
2025-06-03 09:53:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let counter = document.getElementById("counter");
+let btn = document.getElementById("incrementBtn");
+
+btn.addEventListener("click", () =>{
+	let val = parseInt(counter.innerText);
+	alert(val);
+	val = val+1;
+	counter.innerText = val;
+})
